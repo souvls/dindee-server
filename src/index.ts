9def563528b7075b4663d59dcfe1fad8c484e102
souvls <soulixai.vls@gmail.com>
2025-12-18 19:08:47 +0700

    feat: update CORS origin to allow specific frontend URLs and standardize string literals to double quotes

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(morgan(config.nodeEnv === "development" ? "dev" : "combined"));
 app.use(helmet());
 app.use(
   cors({
-    origin: ["https://dindee-web-fe.vercel.app/", "http://localhost:3000"],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
